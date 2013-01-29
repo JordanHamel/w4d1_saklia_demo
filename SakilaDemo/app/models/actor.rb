a5b855d3fdb58356f908fb4b8c7edc46e90a16da
Shaun Mullen <smmullen90@gmail.com>
2013-01-28 17:47:59 -0800

    through some of the film model

--- SakilaDemo/app/models/actor.rb
+++ SakilaDemo/app/models/actor.rb
@@ -18,6 +18,50 @@ class Actor < ActiveRecord::Base
       .limit(10)
   end
 
+  def avg_gross
+    total_gross = 0
+    num_movies = 0
+
+    self.films.each do |film|
+      total_gross += film.gross
+      num_movies += 1
+    end
+
+    total_gross / num_movies
+  end
+
+  def self.avg_gross
+    self
+      .select("actor.*, AVG((film.rental_rate * COUNT(DISTINCT rental.rental_id))) AS actor_avg")
+      .joins(:films)
+      .joins(:rentals)
+      .group("actor.actor_id")
+      .order("actor_avg DESC")
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # actors_with_film = self.includes(:films)
+
+    # actors_with_film.each do |actor|
+    #   total_gross = 0
+    #   num_movies = 0
+    #   actor.films.each do |film|
+    #     total_gross += film.gross
+    #     num_movies += 1
+    #   end
+
+  end
+
   # def self.most_popular(country)
   #   self
   #     .select("actor.*, COUNT(*) AS rental_count")
--- SakilaDemo/app/models/film.rb
+++ SakilaDemo/app/models/film.rb
@@ -11,7 +11,7 @@ class Film < ActiveRecord::Base
   has_many :stores, through: :inventories
   has_many :customers, through: :inventories
   has_many :rentals, through: :customers
-  has_many :payments, through: :rentals
+
 
   def self.biggest_cast
     self
@@ -41,11 +41,41 @@ class Film < ActiveRecord::Base
   end
 
   def self.most_watched
-    self.rentals
+    self
       .select("film.*, COUNT(*) AS rental_count")
       .joins(:rentals)
       .group("film.film_id")
       .order("rental_count DESC")
       .limit(10)
   end
+
+  def self.gross_movies
+    movie_grosses = self
+      .select("film.film_id, film.rental_rate * COUNT(DISTINCT
+        rental.rental_id) AS gross_per_movie")
+      .joins(:rentals)
+      .group("film.film_id")
+      .limit(10)
+
+    gross_sum = 0
+    movie_grosses.each do |film|
+      gross_sum += film.gross_per_movie.to_f
+    end
+
+    gross_sum
+  end
+
+  def gross
+     self.rentals.count * self.rental_rate.to_f
+  end
 end
+
+
+
+
+
+
+
+
+
+
